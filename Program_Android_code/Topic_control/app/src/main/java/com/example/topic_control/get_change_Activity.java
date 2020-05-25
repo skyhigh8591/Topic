71311899b76e0239a8b720ab1b89e0e13ffa3759
skyhigh8591 <taes891@gmail.com>
2020-05-25 15:38:44 +0800

    Topic_control_15

---
+++ Program_Android_code/Topic_control/app/src/main/java/com/example/topic_control/get_change_Activity.java
@@ -0,0 +1,14 @@
+package com.example.topic_control;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class get_change_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_change_);
+    }
+}
