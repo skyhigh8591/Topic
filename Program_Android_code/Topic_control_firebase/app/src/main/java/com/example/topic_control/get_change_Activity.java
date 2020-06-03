6bdd3b6bcbd655b16033d6c4acb6cd1343c93ec3
skyhigh8591 <taes891@gmail.com>
2020-06-03 16:07:51 +0800

    Topic_control_firebase_08

--- Program_Android_code/Topic_control_firebase/app/src/main/java/com/example/topic_control/get_change_Activity.java
+++ Program_Android_code/Topic_control_firebase/app/src/main/java/com/example/topic_control/get_change_Activity.java
@@ -3,11 +3,14 @@ package com.example.topic_control;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class get_change_Activity extends AppCompatActivity {
 
     private Context context;
+    private String RFID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,8 @@ public class get_change_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_get_change_);
 
         context = this;
+        Intent intent = getIntent();
+        RFID = intent.getStringExtra("RFID");
+        Log.d("main","RFID =" + RFID);
     }
 }
